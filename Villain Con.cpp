2ba8930d6c94e9eab640970ddf942584d1590461
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-06-04 11:49:29 +0530

    Create Villain Con.cpp
    Output Status:
    
    Problem Solved Successfully 
     Test Cases Passed:
    17 / 17
    Total Points Scored:
    4/4
    Total Time Taken:
    0.31/1.42
    Your Accuracy:
    100%
    Attempts No.:
    1
    
    The minions are very elitist in nature. If minion x admires minion y, then y thinks too highly of himself and does not admire x back. Also, if x admires y, x also admires everyone that y admires.
    All the minions are going to be present at the Villain Con. They want to make sure that they do not dress in the same color as someone who admires them. 
    There are N minions and M relations between them. The relations are given in a 2D array mat. Each relation is given in xi , yi format where yi admires xi. Find the minimum number of different colours that the minions will be dressing in. 
    
     
    
    Example 1:
    
    Input: 
    N = 5, M = 6
    mat = {{1, 3}, 
           {2, 3}, 
           {3, 4}, 
           {1, 4}, 
           {2, 5}, 
           {3, 5}}
    Output: 3
    Explaination:
    If we assign red colour to 1 and 2,
    green colour to 3, and blue colour to 4 and 5, then
    every minion will have different coloured dresses
    from the one who admires them.
    Example 2:
    
    Input:
    N = 3, M = 2
    mat = {{1,3},{2,3}}
    Output:
    2
    Explanation:
    If we assign red colour to 1 and 2, and green colour to 3, then the condition is satisfied.
    Your Task:
    You do not need to read input or print anything. Your task is to complete the function minColour() which takes N, M and mat[][] as input parameters and returns the minimum number of colours required.
    
     
    
    Expected Time Complexity: O(N+M)
    Expected Auxiliary Space: O(N+M)
    
     
    
    Constraints:
    1 ≤ N ≤ 105
    1 ≤ M ≤ 2*105  
    1 ≤ xi , yi ≤ N

---
+++ Villain Con.cpp
@@ -0,0 +1,60 @@
+Output Status:
+
+Problem Solved Successfully 
+ Test Cases Passed:
+17 / 17
+Total Points Scored:
+4/4
+Total Time Taken:
+0.31/1.42
+Your Accuracy:
+100%
+Attempts No.:
+1
+
+
+class Solution{
+public:
+    int minColour(int N, int M, vector<int> mat[]) {
+       
+        vector <int> adj[N+1], in(N+1);
+        
+        for(int i=0; i<M; i++){
+            int u = mat[i][0], v = mat[i][1];
+            adj[u].push_back(v);
+            
+            in[v]++;
+        }
+        
+        queue <int> q;
+        vector <int> dep(N+1, -1);
+        
+        for(int i=1; i<=N; i++){
+            if(in[i] == 0){
+                q.push(i);
+                dep[i] = 1;
+            }
+        }
+        
+        while(!q.empty()){
+            int v = q.front();
+            q.pop();
+            
+            for(auto& u : adj[v]){
+                in[u]--;
+                dep[u] = max(dep[u], 1 + dep[v]);
+                if(in[u] == 0){
+                    q.push(u);
+                }
+            }
+        }
+        
+        int ans = 1;
+        for(int i=1; i<=N; i++){
+            ans = max(ans, dep[i]);
+        }
+        
+        return ans;
+       
+    }
+};
