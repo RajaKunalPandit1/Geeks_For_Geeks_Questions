ba4ab0e128562200182aec456ab56af74f7688b0
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-09-10 12:25:31 +0530

    Create Ball coloring.cpp
    Output Status : 
    
    Problem Solved Successfully
    Test Cases Passed: 
    1398 /1398
    Total Points Scored: 
    2 /2
    Your Total Score: 
    1360
    Total Time Taken: 
    0.01
    Your Accuracy: 
    50%
    Attempts No.: 
    2
    
    Given n balls . All of them are initially  uncolored . You have to color the balls with two colors RED and BLUE such that there can be atmost 2 positions where a RED ball is touching BLUE ball or vice versa. You have to color all the balls.Find the number of ways in which balls can be colored.
    
    Example 1:
    
    Input: n = 1
    Output: 2
    Explanation: Possible ways to color are {{R},{B}}. 
    So the answer is 2 .
    
    Example 2:
    
    Input: n = 2
    Output: 4
    Explanation: Possible ways to color are 
    {{RB},{BR},{RR},{BB}}.So the answer is 4.
    
    Your Task:  
    You dont need to read input or print anything. Complete the function noOfWays() which takes n as input parameter and returns  the number of ways in which balls can be colored.
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1<= n <=105

---
+++ Ball coloring.cpp
@@ -0,0 +1,24 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+1398 /1398
+Total Points Scored: 
+2 /2
+Your Total Score: 
+1360
+Total Time Taken: 
+0.01
+Your Accuracy: 
+50%
+Attempts No.: 
+2
+
+class Solution {
+  public:
+    unsigned long long int noOfWays(unsigned long long int n){
+       
+       return ((n-1)*n) + 2;
+       
+    }
+};
