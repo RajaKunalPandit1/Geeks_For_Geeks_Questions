a64371f58db1b5e222a25c694c8f2f634ce5f711
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-04-20 12:59:59 +0530

    Create Bheem Wants Ladoos.cpp
    Output Status : 
    
    Problem Solved Successfully
    Test Cases Passed: 
    1115 /1115
    Total Points Scored: 
    4 /4
    Your Total Score: 
    2201
    Total Time Taken: 
    0.37
    Your Accuracy: 
    100%
    Attempts No.: 
    1
    
    Chhota Bheem wants to eat the maximum number of ladoos in Dholakpur on Independence Day. The houses in Dholakpur are arranged in the form of a binary tree and have ladoos the same as their house number. Chhota Bheem is standing at his home initially. 
    Find the maximum ladoos he can eat if he can go to houses within a maximum distance k from his house. The number of ladoos at his home should also be included in the sum.
    
    Note: Every house has distinct ladoos in it. 
    Example 1:
    
    Input:
                       1
                     /    \
                    2      9
                   /      /  \
                  4      5     7
                /   \         /  \
               8     19     20    11
              /     /  \
             30   40   50
    home = 9, K = 1
    Output:
    22
    Explanation:
    Initially Bheem at 9, so sum = 9
    In 2nd move he went to 5, sum=9+5=14
    In 3rd move he went to 7, sum=14+7=21
    In 4th move he went to 1, sum=21+1=22
    So within K distance bheem can get 22 ladoos.  
    
    Example 2:
    
    Input:
                       1
                     /    \
                    2      9
                   /      /  \
                  4      5     7
                /   \         /  \
               8     19     20    11
              /     /  \
             30   40   50
    home = 40, K = 2
    Output:
    113
    Explanation:
    Initially Bheem at 40, so sum = 40
    In 2nd move he went to 19, sum=40+19=59
    In 3rd move he went to 4, sum=59+4=63
    In 4th move he went to 50, sum=63+50=113
    So within K distance bheem can get 113 ladoos.
    
    Your Task:
    You don't need to read input or print anything. Complete the function ladoos() which takes the root of the tree, home, and K  as input parameters and returns the maximum number of ladoos he can eat.
    
    Expected Time Complexity: O(N), where N is no. of nodes
    Expected Space Complexity: O(1)
    
    Constraints:
    1 ≤ N, Home ≤ 105
    1 ≤ K ≤ 20

---
+++ Bheem Wants Ladoos.cpp
@@ -0,0 +1,95 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+1115 /1115
+Total Points Scored: 
+4 /4
+Your Total Score: 
+2201
+Total Time Taken: 
+0.37
+Your Accuracy: 
+100%
+Attempts No.: 
+1
+
+/*
+// node structure:
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+
+
+
+class Solution{
+
+    void parents(Node*root, unordered_map<Node*,Node*>&mp, Node*&node,int home){
+      queue<Node*>q;
+      q.push(root);
+      while(!q.empty()){
+          int n = q.size();
+          for(int i = 0;i<n;i++){
+               Node*temp = q.front();
+               q.pop();
+               if(temp->data == home) node = temp;
+               if(temp->left){
+                   mp[temp->left] = temp;
+                   q.push(temp->left);
+               }
+               if(temp->right){
+                   mp[temp->right] = temp;
+                   q.push(temp->right);
+               }
+          }
+      }
+  }
+    public:
+    int ladoos(Node* root, int home, int k)
+    {
+        // Your code goes here
+        unordered_map<Node*,Node*>parent;
+        Node*h = NULL;
+        parents(root,parent,h,home);
+        queue<Node*>q;
+        unordered_map<Node*,bool>visit;
+     //  Node*n = new Node(home);
+         visit[h] = true;
+         q.push(h);
+         int ans = 0;
+         ans += h->data;
+         int lvl = 0;
+         while(!q.empty()){
+             int size = q.size();
+               if(lvl == k) break;
+               lvl++;
+             for(int i =0 ;i<size;i++){
+                 Node*temp = q.front();
+                 q.pop();
+                 if(temp->left && !visit[temp->left]){
+                     q.push(temp->left);
+                     ans += temp->left->data;
+                     visit[temp->left] = true;
+                 }
+                 if(temp->right && !visit[temp->right]){
+                     q.push(temp->right);
+                     ans += temp->right->data;
+                     visit[temp->right] = true;
+                 }
+                 if(parent[temp] && !visit[parent[temp]]){
+                     q.push(parent[temp]);
+                     ans += parent[temp]->data;
+                     visit[parent[temp]] = true;
+                 }
+             }
+         }
+        return ans;
+    }
+
+};
