e1965bd841a7cb006ad1f8be0e295c38d1e72aa3
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-06-11 14:19:47 +0530

    Create Print Linked List elements.cpp
    Given a linked list. Print all the elements of the linked list.
    
    Note :- End of Line is handled by driver code itself. You just have to end with a single space.
    
    Example 1:
    
    Input:
    N=2
    LinkedList={1 , 2}
    Output:
    1 2
    Explanation:
    The linked list contains two 
    elements 1 and 2.The elements 
    are printed in a single line.
    Example 2:
    
    Input:
    N = 3
    Linked List = { 49, 10, 30}
    Output: 
    49 10 30
    Explanation:
    The linked list contains 3 
    elements 49, 10 and 30. The 
    elements are printed in a single 
    line.
    Your task:
    You don't need to read input or print anything. Your task is to complete the function display() which takes the head of the linked list as input and prints the linked list in a single line.
     
    Expected Time Complexity : O(N)
    Expected Auxiliary Space : O(1)
     
    Constraints :
    1 <= N <= 100

---
+++ Print Linked List elements.cpp
@@ -0,0 +1,27 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+102 /102
+Total Points Scored: 
+1 /1
+Your Total Score: 
+2426
+Total Time Taken: 
+0.01
+Your Accuracy: 
+100%
+Attempts No.: 
+1
+
+void display(Node *head)
+    {
+      
+      Node *curr = head;
+      
+      while(curr != NULL){
+          cout<< curr->data << " ";
+          curr = curr->next;
+      }
+      
+    }
