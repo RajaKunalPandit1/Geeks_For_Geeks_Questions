fe1d789661b722bfcf18ac4cb2ec999211719163
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-06-21 11:01:32 +0530

    Create Reverse Coding.cpp
    You will be given an integer n, your task is to return the sum of all natural number less than or equal to n.
    
    As the answer could be very large, return answer modulo 109+7.
    
    Example 1:
    
    Input:
    n = 6
    Output:
    21
    Explanation:
    1+2+3+4+5+6 = 21
    Example 2:
    
    Input:
    n = 4
    Output:
    10
    Explanation:
    1+2+3+4 = 10
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function sumOfNaturals() which takes a single integer n as input and returns sum of all numbers x upto n, i.e., 1 <= x <= n.
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    0 <= n <= 107

---
+++ Reverse Coding.cpp
@@ -0,0 +1,23 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+1115 /1115
+Total Points Scored: 
+2 /2
+Your Total Score: 
+2548
+Total Time Taken: 
+0.01
+Your Accuracy: 
+50%
+Attempts No.: 
+2
+
+    int sumOfNaturals(int n) {
+        
+        long long int m = n;
+        
+        return ((m*(m+1))/2) % 1000000007;
+
+    }
