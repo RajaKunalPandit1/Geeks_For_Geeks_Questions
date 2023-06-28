21336600723c294953b98459ed6b90fb6aa591fe
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-06-28 10:09:53 +0530

    Create Reverse a Linked List in groups of given size.cpp
    Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
    
    Example 1:
    
    Input:
    LinkedList: 1->2->2->4->5->6->7->8
    K = 4
    Output: 4 2 2 1 8 7 6 5 
    Explanation: 
    The first 4 elements 1,2,2,4 are reversed first 
    and then the next 4 elements 5,6,7,8. Hence, the 
    resultant linked list is 4->2->2->1->8->7->6->5.
    Example 2:
    
    Input:
    LinkedList: 1->2->3->4->5
    K = 3
    Output: 3 2 1 5 4 
    Explanation: 
    The first 3 elements are 1,2,3 are reversed 
    first and then elements 4,5 are reversed.Hence, 
    the resultant linked list is 3->2->1->5->4.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)
    
    Constraints:
    1 <= N <= 105
    1 <= k <= N

---
+++ Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,78 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+1110 /1110
+Total Points Scored: 
+4 /4
+Your Total Score: 
+2609
+Total Time Taken: 
+0.06
+Your Accuracy: 
+50%
+Attempts No.: 
+2
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        
+        if(head == NULL || k == 1) return head;
+        
+        node *dummy = new node(0);
+        dummy->next = head;
+        
+        node *prev = dummy,*curr = dummy,*nex = dummy;
+        
+        int cnt = 0;
+        
+        while(curr->next != NULL){
+            curr = curr->next;
+            cnt++;
+        }
+        
+        while(cnt >= k){
+            
+            curr = prev->next;
+            nex = curr->next;
+            
+            for(int i=1;i<k;i++){
+                
+                curr->next = nex->next;
+                nex->next = prev->next;
+                prev->next = nex;
+                nex = curr->next;
+            }
+            
+            prev = curr;
+            cnt -= k;
+        }
+        
+        if(cnt != 0){
+            prev->next = reverse(curr->next,cnt);
+        }
+        
+        return dummy->next;
+    }
+};
