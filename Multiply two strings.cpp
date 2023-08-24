c79faf16aaca3dff213c1acf4783459e3d4a1343
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2023-08-24 23:11:19 +0530

    Create Multiply two strings.cpp
    Given two numbers as strings s1 and s2. Calculate their Product.
    
    Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers.
    
    Example 1:
    
    Input:
    s1 = "0033"
    s2 = "2"
    Output:
    66
    Example 2:
    
    Input:
    s1 = "11"
    s2 = "23"
    Output:
    253
    Your Task: You don't need to read input or print anything. Your task is to complete the function multiplyStrings() which takes two strings s1 and s2 as input and returns their product as a string.
    
    Expected Time Complexity: O(n1* n2)
    Expected Auxiliary Space: O(n1 + n2); where n1 and n2 are sizes of strings s1 and s2 respectively.
    
    Constraints:
    1 ≤ length of s1 and s2 ≤ 103

---
+++ Multiply two strings.cpp
@@ -0,0 +1,54 @@
+Output Status : 
+
+Problem Solved Successfully
+Test Cases Passed: 
+1120 /1120
+Total Points Scored: 
+4 /4
+Your Total Score: 
+2788
+Total Time Taken: 
+0.15
+Your Accuracy: 
+100%
+Attempts No.: 
+1
+
+    string multiplyStrings(string s1, string s2) {
+             if (s1 =="0"|| s2 =="0")
+return"0";
+
+int sign =1;
+if (s1[0] =='-') {
+sign *= -1;
+s1 = s1.substr(1);
+}
+if (s2[0] =='-') {
+sign *= -1;
+s2 = s2.substr(1);
+}
+
+string ans(s1.length() + s2.length(), '0');
+
+for (int i =s2.length() -1; i >=0; i--) {
+int carry =0;
+for (int j =s1.length() -1; j >=0; j--) {
+int product = (s1[j] -'0') * (s2[i] -'0') + (ans[i + j +1] -'0') + carry;
+carry = product / 10;
+ans[i + j +1] = (product %10) +'0';
+}
+ans[i] += carry;
+}
+
+int start =0;
+while (start <ans.length() -1&&ans[start] =='0') {
+start++;
+}
+
+string result = ans.substr(start);
+if (sign ==-1&& result !="0") {
+result = "-" + result;
+}
+
+return result;
+    }
